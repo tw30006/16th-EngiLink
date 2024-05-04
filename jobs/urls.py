c4a022c3fda85aa69f1d9af94ff7e95d6241ff4e
tw30006 <original.heart.2016@gmail.com>
2024-05-04 15:54:54 +0800

    feat:add Read

--- jobs/urls.py
+++ jobs/urls.py
@@ -1,10 +1,11 @@
 from django.urls import path
 from . import views
-from .views import IndexView, AddView
+from .views import IndexView, AddView, ShowView
 
 app_name = "jobs"
 
 urlpatterns = [
     path("", IndexView.as_view(), name="index"),
     path("create", AddView.as_view(), name="create"),
+    path("<pk>", ShowView.as_view(), name="show"),
 ]
--- jobs/views.py
+++ jobs/views.py
@@ -1,15 +1,16 @@
 from django.shortcuts import render
 from django.urls import reverse_lazy
-from django.views.generic import ListView
-from django.views.generic import TemplateView
+from django.views.generic import ListView, DetailView
 from django.views.generic.edit import CreateView
 from .models import Job
 from .forms import JobForm
 
 
 # 列出所有職缺
-class IndexView(TemplateView):
+class IndexView(ListView):
     template_name = "jobs/index.html"
+    model = Job
+    context_object_name = "jobs"
 
 
 # 新增職缺
@@ -18,3 +19,9 @@ class AddView(CreateView):
     model = Job
     form_class = JobForm
     success_url = reverse_lazy("jobs:index")
+
+
+class ShowView(DetailView):
+    model = Job
+    context_object_name = "job"
+    template_name = "jobs/show.html"
