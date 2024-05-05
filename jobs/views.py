22723aa0015b35da785d5ee27febf8661e444132
tw30006 <original.heart.2016@gmail.com>
2024-05-05 20:59:22 +0800

    fix:layout issues

--- jobs/views.py
+++ jobs/views.py
@@ -5,14 +5,12 @@ from .models import Job
 from .forms import JobForm
 
 
-# 列出所有職缺
 class IndexView(ListView):
     template_name = "jobs/index.html"
     model = Job
     context_object_name = "jobs"
 
 
-# 新增職缺
 class AddView(CreateView):
     template_name = "jobs/create.html"
     model = Job
@@ -20,14 +18,12 @@ class AddView(CreateView):
     success_url = reverse_lazy("jobs:index")
 
 
-# 每一筆的詳細職缺內容
 class ShowView(DetailView):
     model = Job
     context_object_name = "job"
     template_name = "jobs/show.html"
 
 
-# 編輯
 class EditView(UpdateView):
     model = Job
     form_class = JobForm
@@ -36,7 +32,6 @@ class EditView(UpdateView):
     success_url = reverse_lazy("jobs:index")
 
 
-# 軟刪除
 class JobDeleteView(DeleteView):
     model = Job
     success_url = reverse_lazy("jobs:index")
