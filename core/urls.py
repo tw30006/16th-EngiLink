c2bf69c049584df1cdbff41493b3d9df9fd9d76e
Ula Wang <lilingwang32@gmail.com>
2024-05-07 18:09:06 +0800

    Create homepage

--- core/urls.py
+++ core/urls.py
@@ -17,8 +17,10 @@ Including another URLconf
 
 from django.contrib import admin
 from django.urls import path, include
+from django.views.generic import TemplateView
 
 urlpatterns = [
+    path('', TemplateView.as_view(template_name='home.html'), name='home'),
     path('resumes/', include('resume.urls')),
     path('admin/', admin.site.urls),
     path('users/', include('users.urls')),
