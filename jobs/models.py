e6710738808c00e81cfe61ba45a0958a0e7826a8
tw30006 <original.heart.2016@gmail.com>
2024-05-07 12:40:29 +0800

    fix:modify model OPENINGS_CHOICES

--- jobs/models.py
+++ jobs/models.py
@@ -7,18 +7,7 @@ class JobManager(models.Manager):
         return super().get_queryset().filter(deleted_at=None)
 
 
-OPENINGS_CHOICES = [
-    (1, "1"),
-    (2, "2"),
-    (3, "3"),
-    (4, "4"),
-    (5, "5"),
-    (6, "6"),
-    (7, "7"),
-    (8, "8"),
-    (9, "9"),
-    (10, "10"),
-]
+OPENINGS_CHOICES = [(i, str(i)) for i in range(1, 11)]
 
 
 class Job(models.Model):
