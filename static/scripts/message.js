82b54c8b3ca9710b3d127a15952022b847b9a733
JeffreyWu <wu19930628@gmail.com>
2024-05-08 12:00:03 +0800

    let the icon horizontal alignment

--- static/scripts/message.js
+++ static/scripts/message.js
@@ -2,20 +2,15 @@ const Toast = Swal.mixin({
     toast: true,
     position: "top-end",
     showConfirmButton: false,
-    timer: 3000,
+    timer: 2000,
     timerProgressBar: true,
     didOpen: (toast) => {
         toast.onmouseenter = Swal.stopTimer;
         toast.onmouseleave = Swal.resumeTimer;
     },
-    // Add html property to include close button
     html: `
-        <div>
-            <div id="toast-message"></div>
-            <button id="close-toast" class="swal2-close" style="display: block; background-color: transparent; border: none; position: absolute; top: 0; right: 0;">&#x2715;</button>
-        </div>
+        <button id="close-toast" class="swal2-close w-0 h-0" style="display: block; background-color: transparent; border: none; position: absolute; top: 0; right: 0;">&#x2715;</button>
     `,
-    // Event listener for the close button
     didRender: (toast) => {
         const closeButton = toast.querySelector('#close-toast');
         closeButton.addEventListener('click', () => {
