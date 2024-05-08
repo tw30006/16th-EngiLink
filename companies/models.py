9169112ca720b47504c2d8c987e2da22384b17d3
andyhuangdev <ah052691@icloud.com>
2024-05-08 22:26:25 +0800

    feat: add is_valid_tin_number

--- companies/models.py
+++ companies/models.py
@@ -21,6 +21,8 @@ class Company(AbstractUser):
             raise ValidationError("公司名稱格式不正確")
         if not re.match(r'^\d{8}$', self.tin):
             raise ValidationError("統一編號應為 8 位數字")
+        if not self.is_valid_tin_number(self.tin):
+            raise ValidationError("無效的統一編號")
         if not re.match(r'^[\u4e00-\u9fa5]{2,}$', self.user_name):
             raise ValidationError("姓名格式不正確")
         if not re.match(r'^(0\d{1,2}-?\d{6,8})$', self.tel):
@@ -29,3 +31,24 @@ class Company(AbstractUser):
     def save(self, *args, **kwargs):
         self.clean()
         super().save(*args, **kwargs)
+<<<<<<< HEAD
+=======
+
+    def is_valid_tin_number(self, tin):
+           index = [1, 2, 1, 2, 1, 2, 4, 1]
+           arr_tin = list(map(int, tin))
+           multiply_arr = [num * index[i] for i, num in enumerate(arr_tin)]
+           
+           def handle_digits(num):
+               if num >= 10:
+                   tens_digit = num // 10
+                   single_digit = num % 10
+                   return tens_digit + single_digit
+               return num
+           
+           result = sum(handle_digits(num) for num in multiply_arr)
+           
+           if tin[6] == '7':
+               return result % 5 == 0 or (result + 1) % 5 == 0
+           return result % 5 == 0    
+>>>>>>> 15a9785 (feat: add is_valid_tin_number)
