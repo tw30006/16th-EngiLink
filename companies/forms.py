ad1331fd9300cba1fb95a7f76273fc5973f78a09
andyhuangdev <ah052691@icloud.com>
2024-05-08 15:27:51 +0800

    feat: add Comapny data validation

--- companies/forms.py
+++ companies/forms.py
@@ -2,6 +2,12 @@ from django.contrib.auth.forms import UserCreationForm, UserChangeForm
 from .models import Company
 
 class CompanyUserCreationForm(UserCreationForm):
+    def __init__(self, *args, **kwargs):
+        super(CompanyUserCreationForm, self).__init__(*args, **kwargs)
+        if self.data:
+            self.fields['password1'].widget.attrs['value'] = self.data.get('password1', '')
+            self.fields['password2'].widget.attrs['value'] = self.data.get('password2', '')
+
     class Meta(UserCreationForm.Meta):
         model = Company
         fields = ['username', 'email', 'company_name', 'tin', 'user_name', 'tel']
--- companies/models.py
+++ companies/models.py
@@ -1,5 +1,7 @@
 from django.contrib.auth.models import AbstractUser
 from django.db import models
+from django.core.exceptions import ValidationError
+import re
 
 class Company(AbstractUser):
     email = models.EmailField(max_length=255)
@@ -7,7 +9,23 @@ class Company(AbstractUser):
     tin = models.CharField(max_length=8)
     user_name = models.CharField(max_length=100)
     tel = models.CharField(max_length=10)
-
+    
     class Meta:
         verbose_name = 'Company'
         verbose_name_plural = 'Companies'
+
+    def clean(self):
+        if Company.objects.filter(email=self.email).exclude(id=self.id).exists():
+            raise ValidationError("這個電子郵件已經被註冊")
+        if not re.match(r'^[\u4e00-\u9fa5]{5,}$', self.company_name):
+            raise ValidationError("公司名稱格式不正確")
+        if not re.match(r'^\d{8}$', self.tin):
+            raise ValidationError("統一編號應為 8 位數字")
+        if not re.match(r'^[\u4e00-\u9fa5]{2,}$', self.user_name):
+            raise ValidationError("姓名格式不正確")
+        if not re.match(r'^(0\d{1,2}-?\d{6,8})$', self.tel):
+            raise ValidationError("電話號碼格式不正確")
+
+    def save(self, *args, **kwargs):
+        self.clean()
+        super().save(*args, **kwargs)
