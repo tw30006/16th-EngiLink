8561472ba31e589815b976fa8fb6b7a107fdc744
Ula Wang <lilingwang32@gmail.com>
2024-05-08 12:13:01 +0800

    update homepage

--- core/urls.py
+++ core/urls.py
@@ -20,7 +20,7 @@ from django.urls import path, include
 from django.views.generic import TemplateView
 
 urlpatterns = [
-    path('', TemplateView.as_view(template_name='home.html'), name='home'),
+    path('', TemplateView.as_view(template_name='home/home.html'), name='home'),
     path('resumes/', include('resume.urls')),
     path('admin/', admin.site.urls),
     path('users/', include('users.urls')),
