4f78bf57890eff7c5f3a3862edc9fca7017c53e6
andyhuangdev <ah052691@icloud.com>
2024-05-09 15:13:54 +0800

    feat: add connection with Company and Jobs

--- companies/urls.py
+++ companies/urls.py
@@ -1,11 +1,12 @@
-from django.urls import path
+from django.urls import path, include
 from .views import IndexView, SignUpView, ProfileUpdateView, ProfileView, LogInView, LogOutView
-from django.contrib.auth import views
+from jobs import views
 
 urlpatterns = [
     path('', IndexView.as_view(), name='index'),
     path('signup/', SignUpView.as_view(), name='signup'),
     path('login/', LogInView.as_view(), name='login'),
+    path('jobs/', views.IndexView.as_view(), name='jobs'),
     path('logout/', LogOutView.as_view(), name='logout'),
     path('edit/', ProfileUpdateView.as_view(), name='edit'),
     path('show/<pk>/', ProfileView.as_view(), name='profile'),
