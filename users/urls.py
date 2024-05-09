04a019a56816082d6123deb592140e37b6060cd6
andyhuangdev <ah052691@icloud.com>
2024-05-09 15:32:27 +0800

    feat: add connection with User and Resumes

--- users/urls.py
+++ users/urls.py
@@ -1,8 +1,11 @@
 from django.urls import path
 from .views import SignupView, UserIndexView, ProfileUpdateView, UserProfileView, SigninView, SignoutView
 from django.contrib.auth import views as auth_views
+from resume.views import ResumeArea
+
 urlpatterns = [
     path("", UserIndexView.as_view(), name="userindex"),
+    path("resume/", ResumeArea.as_view(), name="resume"),
     path("signup/", SignupView.as_view(), name="users_signup"),
     path("edit/", ProfileUpdateView.as_view(template_name="users/edit.html"), name="users_edit"),
     path("signin/", SigninView.as_view(), name="users_signin"),
