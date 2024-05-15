66edc02e9d935dc68413e8985efc388880723b74
andyhuangdev <ah052691@icloud.com>
2024-05-15 16:53:43 +0800

    feat: download resume pdf

--- resumes/views.py
+++ resumes/views.py
@@ -1,12 +1,15 @@
 from django.urls import reverse_lazy
+from django.http import HttpResponse
+from django.shortcuts import get_object_or_404
 from django.views.generic import TemplateView,ListView,CreateView,UpdateView,DeleteView
+from django.contrib.auth.mixins import LoginRequiredMixin
 from .forms import ResumeForm
 from .models import Resume
-from django.shortcuts import get_object_or_404
-from django.contrib.auth.mixins import LoginRequiredMixin
 from educations.models import Education
 from works.models import Work
 from projects.models import Project
+from weasyprint import HTML, CSS
+from django.template.loader import render_to_string
 
 
 
@@ -31,7 +34,6 @@ class ResumeListView(ListView):
         context['resume'] = resume
         return context
 
-
 class ResumeCreateView(LoginRequiredMixin, CreateView):
     model = Resume
     form_class = ResumeForm
@@ -47,8 +49,6 @@ class ResumeCreateView(LoginRequiredMixin, CreateView):
         form.instance.user = self.request.user
         return super().form_valid(form)
 
-
-
 class ResumeUpdateView(UpdateView):
     model = Resume
     form_class = ResumeForm
@@ -60,11 +60,9 @@ class ResumeUpdateView(UpdateView):
         resume.save()
         return super().form_valid(form)
 
-
 class ResumeDeleteView(DeleteView):
     model = Resume
     success_url = reverse_lazy('resumes:index')
-
 
 class TotalListView(ListView):
     template_name = 'resumes/total.html'
@@ -86,3 +84,22 @@ class TotalListView(ListView):
         }
         return total_data
 
+def generate_resume_pdf(request, resume_id):
+    resume = get_object_or_404(Resume, pk=resume_id)
+    user = resume.user
+
+    total_data = {
+        'resume_data': [resume],
+        'education_data': Education.objects.filter(resume=resume),
+        'work_data': Work.objects.filter(resume=resume),
+        'project_data': Project.objects.filter(resume=resume),
+    }
+
+    html_string = render_to_string('pdf_template.html', {'user': user, 'total_data': total_data})
+
+    html = HTML(string=html_string, base_url=request.build_absolute_uri('/'))
+    result = html.write_pdf(stylesheets=[CSS(string='@page { size: A4; margin: 1cm }')])
+
+    response = HttpResponse(result, content_type='application/pdf')
+    response['Content-Disposition'] = f'attachment; filename="resume_{user.username}.pdf"'
+    return response
