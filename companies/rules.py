3b71175b91adc880b66033caad6229b7fb1d0cb5
tw30006 <original.heart.2016@gmail.com>
2024-05-21 17:47:40 +0800

    fix:remove def

--- companies/rules.py
+++ companies/rules.py
@@ -4,9 +4,6 @@ import rules
 def is_company(custom_user):
     return custom_user.user_type == 2
 
-@rules.predicate
-def is_user(custom_user):
-    return custom_user.user_type == 1
 
 rules.add_perm('companies.detail_company',is_company)
 rules.add_perm('companies.home_company',is_company)
--- jobs/rules.py
+++ jobs/rules.py
@@ -4,8 +4,5 @@ import rules
 def is_company(custom_user):
     return custom_user.user_type == 2
 
-@rules.predicate
-def is_user(custom_user):
-    return custom_user.user_type == 1
 
 rules.add_perm('jobs.show_job',is_company)
