029ffb38b7e3210c5678e142338e4ef7d896bc7c
tw30006 <original.heart.2016@gmail.com>
2024-05-26 17:34:33 +0800

    fix: jobs rules.py

--- jobs/rules.py
+++ jobs/rules.py
@@ -5,8 +5,8 @@ def is_company(custom_user):
     return custom_user.user_type == 2
 
 @rules.predicate
-def is_current_company(user,company):
-    return user == company.custom_user
+def is_current_company(company_user,company):
+    return company_user == company.custom_user
 
 
 rules.add_perm('jobs.show_job',is_company)
