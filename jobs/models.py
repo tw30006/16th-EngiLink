2d677055101162223a94175045abcc5955800327
tw30006 <original.heart.2016@gmail.com>
2024-05-30 15:08:48 +0800

    fix:remove address verification

--- jobs/models.py
+++ jobs/models.py
@@ -1,10 +1,8 @@
 from django.db import models
 from django.utils import timezone
 from resumes.models import Resume
-from django.core.validators import MinValueValidator, MaxValueValidator
-from django.core.exceptions import ValidationError
+from django.core.validators import MinValueValidator
 from companies.models import Company
-import re
 from users.models import CustomUser
 
 
@@ -16,15 +14,8 @@ class JobManager(models.Manager):
 OPENINGS_CHOICES = [(i, str(i)) for i in range(1, 11)]
 
 
-def validate_letters_and_chinese(value):
-    if not re.match(r"^[a-zA-Z\u4e00-\u9fa5]+$", value):
-        raise ValidationError("欄位只能填寫中文或英文")
-
-
 def validate_taiwan_address(value):
-    address_re = r"^(台北市|新北市|桃園市|台中市|台南市|高雄市|基隆市|新竹市|新竹縣|苗栗縣|彰化縣|南投縣|雲林縣|嘉義市|嘉義縣|屏東縣|宜蘭縣|花蓮縣|台東縣|澎湖縣|金門縣|連江縣)[\u4e00-\u9fa5]+(?:區|市|鎮|鄉)?[\u4e00-\u9fa5]*(?:路|街|巷)[\u4e00-\u9fa5]*[0-9]*號(?:之[0-9]+)?(?:[-0-9樓]+)?$"
-    if not re.match(address_re, value):
-        raise ValidationError("請輸入正確的地址")
+    pass
 
 
 class Job(models.Model):
@@ -38,13 +29,10 @@ class Job(models.Model):
     resumes = models.ManyToManyField(Resume, through="Job_Resume")
     users = models.ManyToManyField(CustomUser, through="User_Job")
     experience = models.PositiveIntegerField(
-        validators=[
-            MinValueValidator(0),
-            MaxValueValidator(50, message="年資必須低於50"),
-        ]
+        validators=[MinValueValidator(0)]
     )
     salary = models.PositiveIntegerField(blank=True, validators=[MinValueValidator(0)])
-    address = models.CharField(max_length=250, validators=[validate_taiwan_address])
+    address = models.CharField(max_length=250, default="")
     description = models.TextField(default="")
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
