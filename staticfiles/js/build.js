c9842f2349fb27178892e601cfd141f3455cd3c0
JeffreyWu <wu19930628@gmail.com>
2024-06-01 20:41:41 +0800

    Update .gitignore to ignore collected static files and remove them from version control

--- staticfiles/js/build.js
+++
@@ -1,7 +0,0 @@
-const esbuild = require('esbuild');
-
-esbuild.build({
-    entryPoints: ["static/js/entry.js"],
-    bundle: true,
-    outfile: "static/js/bundle.js",
-}).catch(() => process.exit(1));
--- staticfiles/js/entry.js
+++
@@ -1,8 +0,0 @@
-import "./sweetalert.js";
-import 'htmx.org';
-import "./drag.js";
-import "./alpine.js";
-import "./notice.js";
-import "./preview.js";
-import "./home.js";
-import "./font.js";
