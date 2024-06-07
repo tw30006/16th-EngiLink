58dcea2d529a3284128f7af556e4fe7f9ba3f2cc
andyhuangdev <ah052691@icloud.com>
2024-06-07 11:44:17 +0800

    feat: permission management of Jobs

--- jobs/rules.py
+++ jobs/rules.py
@@ -1,13 +1,21 @@
 import rules
 
+
 @rules.predicate
 def is_company(custom_user):
     return custom_user.user_type == 2
 
+
 @rules.predicate
-def is_current_company(company_user,company):
+def is_current_company(company_user, company):
     return company_user == company.custom_user
 
 
-rules.add_perm('jobs.show_job',is_company)
-rules.add_rule('is_current_company',is_current_company)
+@rules.predicate
+def is_job_owner(user, job):
+    return user == job.company.custom_user
+
+
+rules.add_perm("jobs.show_job", is_company)
+rules.add_rule("is_current_company", is_current_company)
+rules.add_rule("is_job_owner", is_job_owner)
