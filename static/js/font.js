66361de2d0b631cae97eff6c199bda8658d393fc
yuling515 <yuling515@gmail.com>
2024-06-07 14:46:08 +0800

    feat: add icon to static/js/font.js

--- static/js/font.js
+++ static/js/font.js
@@ -1,8 +1,9 @@
 import { library, dom, config } from "@fortawesome/fontawesome-svg-core";
 import { faLocationDot, faSackDollar, faBars, faFire, faHeart, faBriefcase, faArrowUpRightFromSquare } from '@fortawesome/free-solid-svg-icons';
 import { faThumbsUp } from '@fortawesome/free-regular-svg-icons';
+import { faGoogle } from '@fortawesome/free-brands-svg-icons';
 
 config.mutateApproach = "sync";
-library.add(faLocationDot, faSackDollar, faBars, faFire, faHeart, faBriefcase, faArrowUpRightFromSquare, faThumbsUp);
+library.add(faLocationDot, faSackDollar, faBars, faFire, faHeart, faBriefcase, faArrowUpRightFromSquare, faThumbsUp,faGoogle);
 
 dom.watch();
