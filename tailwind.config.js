8b295307059ec03395230cd3c7893922124eae02
tw30006 <original.heart.2016@gmail.com>
2024-06-08 11:20:36 +0800

    fix: modify user color

--- tailwind.config.js
+++ tailwind.config.js
@@ -11,6 +11,10 @@ module.exports = {
       scrollMargin: {
         '16': '4rem',
       },
+      colors: {
+        'user-blue': '#0D4295',
+        'company-green': '#008F77',
+      },
     },
   },
   plugins: [],
