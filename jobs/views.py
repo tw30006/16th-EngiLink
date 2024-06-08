e413c091f1c0f6a893aefde65ffe2e4f3c7c61c7
andyhuangdev <ah052691@icloud.com>
2024-06-08 19:25:59 +0800

    fix: change search bar's style of company_list and add search bar's of job_list

--- jobs/views.py
+++ jobs/views.py
@@ -5,31 +5,31 @@ from django.shortcuts import render, get_object_or_404
 from django.urls import reverse
 from django.views.generic import ListView, DetailView
 from django.views.generic.edit import CreateView, UpdateView, DeleteView
-from companies.models import Company
-from jobs.models import User_Job
+from django.shortcuts import render, get_object_or_404
 from .models import Job
 from .forms import JobForm
+from companies.models import Company
 import rules
+from jobs.models import User_Job
 
-
-class IndexView(PermissionRequiredMixin, ListView):
+class IndexView(PermissionRequiredMixin,ListView):
     template_name = "jobs/index.html"
     model = Job
     context_object_name = "jobs"
     permission_required = "jobs.show_job"
 
     def dispatch(self, request, *args, **kwargs):
-        pk = self.kwargs.get("pk")
+        pk = self.kwargs.get('pk')
         company = get_object_or_404(Company, pk=pk)
-        if not rules.test_rule("is_current_company", request.user, company):
+        if not rules.test_rule('is_current_company',request.user,company):
             return HttpResponseForbidden()
-        return super().dispatch(request, *args, **kwargs)
+        return super().dispatch(request,*args, **kwargs)
 
     def get_queryset(self):
-        pk = self.kwargs.get("pk")
+        pk = self.kwargs.get('pk')
         company = get_object_or_404(Company, pk=pk)
         return Job.objects.filter(company=company)
-
+        
 
 class AddView(CreateView):
     template_name = "jobs/create.html"
@@ -37,11 +37,11 @@ class AddView(CreateView):
     form_class = JobForm
 
     def dispatch(self, request, *args, **kwargs):
-        pk = self.kwargs.get("pk")
+        pk = self.kwargs.get('pk')
         company = get_object_or_404(Company, pk=pk)
-        if not rules.test_rule("is_current_company", request.user, company):
+        if not rules.test_rule('is_current_company',request.user,company):
             return HttpResponseForbidden()
-        return super().dispatch(request, *args, **kwargs)
+        return super().dispatch(request,*args, **kwargs)
 
     def get_form_kwargs(self):
         kwargs = super().get_form_kwargs()
@@ -62,13 +62,6 @@ class ShowView(DetailView):
     model = Job
     context_object_name = "job"
 
-    def dispatch(self, request, *args, **kwargs):
-        job = self.get_object()
-        if not rules.test_rule('is_job_owner', request.user, job):
-            return HttpResponseForbidden()
-        return super().dispatch(request, *args, **kwargs)
-
-
 
 class EditView(UpdateView):
     model = Job
@@ -81,18 +74,10 @@ class EditView(UpdateView):
         messages.success(self.request, "更新成功")
         self.success_url = reverse("companies:jobs", kwargs={"pk": company_id})
         return super().form_valid(form)
-    
-    def dispatch(self, request, *args, **kwargs):
-        job = self.get_object()
-        if not rules.test_rule('is_job_owner', request.user, job):
-            return HttpResponseForbidden()
-        return super().dispatch(request, *args, **kwargs)
-
-
 
 class JobDeleteView(DeleteView):
     model = Job
-
+    
     def get_success_url(self):
         pk = self.object.company.pk
         messages.success(self.request, "刪除成功")
@@ -107,7 +92,7 @@ class JobDeleteView(DeleteView):
 
 
 class PublishView(DetailView):
-    model = Job
+    model=Job
     context_object_name = "job"
     template_name = "jobs/job.html"
 
@@ -115,43 +100,43 @@ class PublishView(DetailView):
         self.object = self.get_object()
         self.object.is_published = not self.object.is_published
         self.object.save()
-        return render(request, "jobs/job.html", {"job": self.object})
-
+        return render(request, "jobs/job.html", {"job":self.object})
 
 class JobListView(ListView):
     model = Job
-    template_name = "jobs/list.html"
-    context_object_name = "jobs"
-
+    template_name = 'jobs/list.html'
+    context_object_name = 'jobs'
+    
     def get_context_data(self, **kwargs):
         context = super().get_context_data(**kwargs)
         job_list = []
-        for job in context["jobs"]:
+        for job in context['jobs']:
             company = job.company
             job_info = {
-                "title": job.title,
-                "company_name": company.company_name,
-                "company_logo_url": company.logo.url if company.logo else "",
-                "company_banner_url": company.banner.url if company.banner else "",
-                "address": job.address,
-                "salary": job.salary,
-                "description": job.description,
-                "id": job.id,
+                'title': job.title,
+                'company_name': company.company_name,
+                'company_logo_url': company.logo.url if company.logo else '',
+                'company_banner_url': company.banner.url if company.banner else '',
+                'address': job.address,
+                'salary': job.salary,
+                'description': job.description,
+                'id': job.id
             }
             job_list.append(job_info)
-        context["jobs"] = job_list
+        context['jobs'] = job_list
         if self.request.user.is_authenticated:
-            user_jobs = User_Job.objects.filter(user=self.request.user).values_list(
-                "job_id", flat=True
-            )
-            context["user_jobs"] = list(user_jobs)
+            user_jobs = User_Job.objects.filter(user=self.request.user).values_list('job_id', flat=True)
+            context['user_jobs'] = list(user_jobs)
         return context
     
     def get_queryset(self):
-        return Job.objects.filter(is_published=True)
-
+        queryset = Job.objects.all()
+        search_keyword = self.request.GET.get("q")
+        if search_keyword:
+            queryset = queryset.filter(title__icontains=search_keyword)
+        return queryset
 
 class JobDetailView(DetailView):
     model = Job
-    template_name = "jobs/detail.html"
-    context_object_name = "job"
+    template_name = 'jobs/detail.html'
+    context_object_name = 'job'
