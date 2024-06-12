1833824524efb435b047166021b92d9400b3f03b
yuling515 <yuling515@gmail.com>
2024-06-12 17:25:28 +0800

    feat: static/js/font.js

--- static/js/font.js
+++ static/js/font.js
@@ -2,8 +2,9 @@ import { library, dom, config } from "@fortawesome/fontawesome-svg-core";
 import { faLocationDot, faSackDollar, faBars, faFire, faHeart, faBriefcase, faArrowUpRightFromSquare } from '@fortawesome/free-solid-svg-icons';
 import { faThumbsUp } from '@fortawesome/free-regular-svg-icons';
 import { faGoogle } from '@fortawesome/free-brands-svg-icons';
+import { faGithub } from '@fortawesome/free-brands-svg-icons';
 
 config.mutateApproach = "sync";
-library.add(faLocationDot, faSackDollar, faBars, faFire, faHeart, faBriefcase, faArrowUpRightFromSquare, faThumbsUp,faGoogle);
+library.add(faLocationDot, faSackDollar, faBars, faFire, faHeart, faBriefcase, faArrowUpRightFromSquare, faThumbsUp,faGoogle, faGithub);
 
 dom.watch();
